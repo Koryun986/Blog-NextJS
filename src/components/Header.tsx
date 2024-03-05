caa20e767a3c372eb90851e04022d3351c3b7d7a
KoryunWeConstruct <koryun986@gmail.com>
2024-03-05 19:28:37 +0400

    chore(link hover) add hover on link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ const Header = ({posts, setPosts}: HeaderProps) => {
         <div className="sm:flex justify-between items-center">
             <h1 className="font-bold text-6xl">Blog</h1>
             <PostsSearch posts={posts} setPosts={setPosts} />
-            <Link href={"posts/create"} className="mt-5 rounded-lg text-white py-2 px-3 bg-blue-500">Create Post</Link>
+            <Link href={"posts/create"} className="mt-5 rounded-lg text-white py-2 px-3 bg-blue-500 hover:bg-blue-600">Create Post</Link>
         </div>
     );
 };
